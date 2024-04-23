c0f7d384e8783695165719cf159e6398bfd8b6b8
XanbabaF <xanbabaf@gmail.com>
2024-04-23 18:34:13 +0400

    *

--- WeatherApp/MainForm.cs
+++ WeatherApp/MainForm.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace WeatherApp;
 
 public partial class MainForm : Form
 {
-    public static event Action<Weather>? WeatherRequested;
+    public static event Action<WeatherLocation>? WeatherRequested;
 
     private readonly WeathersManager _weathersManager = new();
 
@@ -51,7 +53,30 @@ public partial class MainForm : Form
     {
         var weatherInfoForm = new WeatherInfoForm();
         WeatherRequested?.Invoke(
-            _weathersManager.GetWeatherLocation(weatherLocationsCheckListBox.SelectedIndex).Weather);
+            _weathersManager.GetWeatherLocation(weatherLocationsCheckListBox.SelectedIndex));
         weatherInfoForm.ShowDialog();
+    }
+
+    private void importButton_Click(object sender, EventArgs e)
+    {
+        var dialog = new OpenFileDialog();
+        dialog.Filter = @"wthr files (*.wthr)|*.wthr";
+        dialog.ShowDialog();
+        if (string.IsNullOrWhiteSpace(dialog.FileName))
+        {
+            return;
+        }
+
+        try
+        {
+            var weatherLocation = JsonConvert.DeserializeObject<WeatherLocation>(File.ReadAllText(dialog.FileName));
+            if (weatherLocation == null) throw new Exception();
+            _weathersManager.ImportWeatherLocation(weatherLocation);
+            MessageBox.Show(@"The weather was successfully imported");
+        }
+        catch (Exception)
+        {
+            MessageBox.Show(@"An Error occured while importing the weather");
+        }
     }
 }
